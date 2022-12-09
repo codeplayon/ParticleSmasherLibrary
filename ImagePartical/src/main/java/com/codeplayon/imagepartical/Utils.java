7ff85807ae954cf0a674087b08cb87489aa88c4b
codeplayon <codeplayon@gmail.com>
2022-12-09 08:11:05 +0530

    Initial commit

---
+++ ImagePartical/src/main/java/com/codeplayon/imagepartical/Utils.java
@@ -0,0 +1,12 @@
+package com.codeplayon.imagepartical;
+import android.content.res.Resources;
+
+public class Utils {
+
+
+    private static final float DENSITY = Resources.getSystem().getDisplayMetrics().density;
+    public static int dp2Px(int dp) {
+        return Math.round(dp * DENSITY);
+    }
+
+}
